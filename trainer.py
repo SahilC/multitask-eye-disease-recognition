299f74ec3ec8cd6c413c9b471a8e58a15d3d9f3e
Sahil C <sahilc.2200@gmail.com>
2019-11-04 03:38:52 -0800

    Bugfix

--- trainer.py
+++ trainer.py
@@ -45,7 +45,7 @@ class BaseTrainer(object):
 
 class MultiTaskTrainer(BaseTrainer):
     def __init__(self, model, optimizer, scheduler, criterion, tasks, epochs, lang, print_every = 100, min_val_loss = 100):
-        super(Trainer, self).__init__(model, optimizer, scheduler, criterion, epochs, print_every, min_val_loss)
+        super(MultiTaskTrainer, self).__init__(model, optimizer, scheduler, criterion, epochs, print_every, min_val_loss)
         self.lang = lang
         self.tasks = tasks
         self.save_location_dir = os.path.join('models', '_'.join(str(t) for t in self.tasks) +'-'+ str(datetime.now()).replace(' ',''))
